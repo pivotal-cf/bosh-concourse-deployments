ba36014924bc1547d13c9dde0f84a8e8e2a8b44e
Chris Dutra <cdutra@pivotal.io>
2017-07-31 17:37:49 -0700

    Add core team's concourse pipeline
    [#146488783](https://www.pivotaltracker.com/story/show/146488783)
    
    Signed-off-by: Danny Berger <dberger@pivotal.io>

---
+++ terraform/allow_jumpbox/allow_jumpbox.tf
@@ -0,0 +1,48 @@
+// INPUTS
+variable "project_id" {}
+variable "gcp_credentials_json" {}
+variable "region" {
+  default = "us-west1"
+}
+variable "allow_ssh_access_to_jumpbox" {
+  default = 0
+  description = "Set to `1` to allow SSH from `trusted_cidrs` to the jumpbox. This should only be done temporarily for debugging or tunneling."
+}
+variable "allow_mbus_access_to_jumpbox" {
+  default = 0
+  description = "Set to `1` to allow mbus traffic on 6868 from `trusted_cidrs` to the jumpbox. This should only be done temporarily to upgrade the jumpbox."
+}
+variable "trusted_cidrs" {
+  type = "list"
+}
+variable "network" {
+  default = "concourse"
+}
+variable "name" {}
+
+provider "google" {
+  project     = "${var.project_id}"
+  credentials = "${var.gcp_credentials_json}"
+  region      = "${var.region}"
+}
+
+// allow BOSH ports to allow SSH tunneling from `trusted_cidrs` to Director via Jumpbox
+// This resource will not created by default, set `allow_ssh_access_to_jumpbox=1` to enable
+resource "google_compute_firewall" "jumpbox-ssh" {
+  count = "${var.allow_ssh_access_to_jumpbox}"
+
+  name    = "${var.name}-ssh"
+  network = "${var.network}"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["${var.trusted_cidrs}"]
+  target_tags = ["${var.network}-jumpbox"]
+}
