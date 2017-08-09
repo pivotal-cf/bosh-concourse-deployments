ba24fd5598465c601bdd00d4f9e9ddc9b3cbaf29
Danny Berger <dberger@pivotal.io>
2017-08-09 15:16:45 -0700

    admin: Make connect-bosh more resilient
    Signed-off-by: Chris Dutra <cdutra@pivotal.io>

--- connect-bosh.sh
+++ connect-bosh.sh
@@ -3,29 +3,33 @@ set -eu
 
 secure_note="${1?'Provide the LastPass secure note name'}"
 
-tmp_dir="$( mktemp -d /tmp/jumpbox-XXXXXX)"
-lpass_note="$( lpass show --note "$secure_note" )"
+lpass_note="$( set -eu ; lpass show --note "$secure_note" )"
 
 creds() {
   local path=${1?'Path is required.'}
   bosh2 int <( echo "$lpass_note" ) --path /"$path"
 }
 
-# Download director CA Cert
-creds bosh_ca_cert > "$tmp_dir/ca_cert.pem"
-trap "{ rm -rf '$tmp_dir' }" EXIT
+jumpbox_host="$( set -eu ; creds jumpbox_host )"
+jumpbox_user="$( set -eu ; creds jumpbox_ssh_user )"
 
-# Download jumpbox SSH key
+tmp_dir="$( mktemp -d /tmp/jumpbox-XXXXXX)"
+
+cleanup() {
+  [[ ! -e "$tmp_dir/tunnel-socket" ]] || ssh -S "$tmp_dir/tunnel-socket" -O exit "$jumpbox_user@$jumpbox_host"
+  rm -rf "$tmp_dir"
+}
+
+trap cleanup EXIT
+
+# director CA Cert
+creds bosh_ca_cert > "$tmp_dir/ca_cert.pem"
+# jumpbox SSH key
 creds jumpbox_ssh_key > "$tmp_dir/vcap.pem"
 chmod 600 "$tmp_dir/vcap.pem"
-
-# Download director client and client secret
+# director client and client secret
 export BOSH_CLIENT="$( creds bosh_client )"
 export BOSH_CLIENT_SECRET="$( creds bosh_client_secret )"
-
-jumpbox_host="$( creds jumpbox_host )"
-jumpbox_user="$( creds jumpbox_ssh_user )"
-
 export BOSH_ENVIRONMENT=10.0.0.6
 export BOSH_CA_CERT="$tmp_dir/ca_cert.pem"
 
@@ -33,7 +37,6 @@ export BOSH_CA_CERT="$tmp_dir/ca_cert.pem"
 ssh \
   -M -S "${tmp_dir}/tunnel-socket" \
   -i "$tmp_dir/vcap.pem" -fnNT -D 1080 "$jumpbox_user@$jumpbox_host"
-trap "{ ssh -S '$tmp_dir/tunnel-socket' -O exit '$jumpbox_user@$jumpbox_host'; rm -rf '$tmp_dir'; }" EXIT
 export BOSH_ALL_PROXY=socks5://localhost:1080
 
 $SHELL --rcfile <(cat ~/.bashrc - <<'EOF'
