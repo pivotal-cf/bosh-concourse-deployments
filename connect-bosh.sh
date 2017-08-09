fb3ba926bae3bc062c5d7066c91f2e8b337beeb7
Danny Berger <dberger@pivotal.io>
2017-08-09 14:51:57 -0700

    admin: cleanup connect-bosh
    - avoid BOSH GW
    - pull jumpbox host from lpass
    - do NOT call lpass unnecessarily
    
    Signed-off-by: Chris Dutra <cdutra@pivotal.io>

--- connect-bosh.sh
+++ connect-bosh.sh
@@ -1,43 +1,39 @@
 #!/bin/bash
 set -eu
 
-jumpbox_ip="${1?'Provide the jumpbox IP Address'}"
-secure_note="${2?'Provide the LastPass secure note name'}"
+secure_note="${1?'Provide the LastPass secure note name'}"
 
 tmp_dir="$( mktemp -d /tmp/jumpbox-XXXXXX)"
+lpass_note="$( lpass show --note "$secure_note" )"
+
+creds() {
+  local path=${1?'Path is required.'}
+  bosh2 int <( echo "$lpass_note" ) --path /"$path"
+}
+
 # Download director CA Cert
-bosh2 int \
-  <( lpass show --note "$secure_note" ) \
-  --path /bosh_ca_cert \
-  > "$tmp_dir/ca_cert.pem"
+creds bosh_ca_cert > "$tmp_dir/ca_cert.pem"
 trap "{ rm -rf '$tmp_dir' }" EXIT
 
 # Download jumpbox SSH key
-bosh2 int \
-  <( lpass show --note "$secure_note" ) \
-  --path /jumpbox_ssh_key \
-  > "$tmp_dir/vcap.pem"
+creds jumpbox_ssh_key > "$tmp_dir/vcap.pem"
 chmod 600 "$tmp_dir/vcap.pem"
 
 # Download director client and client secret
-export BOSH_CLIENT="$( bosh2 int \
-  <( lpass show --note "$secure_note" ) \
-  --path /bosh_client )"
-export BOSH_CLIENT_SECRET="$( bosh2 int \
-  <( lpass show --note "$secure_note" ) \
-  --path /bosh_client_secret )"
+export BOSH_CLIENT="$( creds bosh_client )"
+export BOSH_CLIENT_SECRET="$( creds bosh_client_secret )"
+
+jumpbox_host="$( creds jumpbox_host )"
+jumpbox_user="$( creds jumpbox_ssh_user )"
 
 export BOSH_ENVIRONMENT=10.0.0.6
 export BOSH_CA_CERT="$tmp_dir/ca_cert.pem"
-export BOSH_GW_USER=jumpbox
-export BOSH_GW_HOST=10.0.0.6
-export BOSH_GW_PRIVATE_KEY="$tmp_dir/vcap.pem"
 
 # SSH tunnel through the jumpbox
 ssh \
   -M -S "${tmp_dir}/tunnel-socket" \
-  -i "$tmp_dir/vcap.pem" -fnNT -D 1080 jumpbox@"$jumpbox_ip"
-trap "{ ssh -S '$tmp_dir/tunnel-socket' -O exit jumpbox@'$jumpbox_ip'; rm -rf '$tmp_dir'; }" EXIT
+  -i "$tmp_dir/vcap.pem" -fnNT -D 1080 "$jumpbox_user@$jumpbox_host"
+trap "{ ssh -S '$tmp_dir/tunnel-socket' -O exit '$jumpbox_user@$jumpbox_host'; rm -rf '$tmp_dir'; }" EXIT
 export BOSH_ALL_PROXY=socks5://localhost:1080
 
 $SHELL --rcfile <(cat ~/.bashrc - <<'EOF'
