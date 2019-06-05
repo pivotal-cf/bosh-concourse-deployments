24c88a2d799e663c282d0aa1b45a5467bb8f0455
James Myers <jmyers@pivotal.io>
2019-06-04 17:24:04 -0700

    Temporarily lock google provider to 2.7.0
    Co-authored-by: Belinda Liu <bliu@pivotal.io>

--- terraform/concourse/main.tf
+++ terraform/concourse/main.tf
@@ -2,6 +2,7 @@ provider "google" {
   project     = "${var.project_id}"
   credentials = "${var.gcp_credentials_json}"
   region      = "${var.region}"
+  version     = "2.7.0"
 }
 
 resource "google_compute_network" "bosh" {
