f819aa0b0fea149f6cc392a452007ed6ff48a679
Aakash Shah <ashah@pivotal.io>
2019-06-12 11:35:18 -0700

    Fix outputs being string, not arrays
    * Fixes "update-cloud-config" task having double-arrays
    
    [#166477563](https://www.pivotaltracker.com/story/show/166477563)
    
    Co-authored-by: Rebecca Putinski <rputinski@pivotal.io>

--- terraform/modules/concourse/concourse.tf
+++ terraform/modules/concourse/concourse.tf
@@ -101,15 +101,15 @@ output "external_ip" {
 }
 
 output "atc_tag" {
-  value = google_compute_firewall.bosh-atc-to-db.source_tags
+  value = element(tolist(google_compute_firewall.bosh-atc-to-db.source_tags), 0)
 }
 
 output "worker_tag" {
-  value = google_compute_firewall.concourse-worker.source_tags
+  value = element(tolist(google_compute_firewall.concourse-worker.source_tags), 0)
 }
 
 output "db_tag" {
-  value = google_compute_firewall.bosh-atc-to-db.target_tags
+  value = element(tolist(google_compute_firewall.bosh-atc-to-db.target_tags), 0)
 }
 
 output "target_pool" {
