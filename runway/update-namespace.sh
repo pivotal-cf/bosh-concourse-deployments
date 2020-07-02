56cbd1b9e11fa0d468086f2570adcbf8c1ecb318
Ruben Koster <rkoster@starkandwayne.com>
2020-07-02 15:13:57 +0200

    Added script for updating our runway namespace

---
+++ runway/update-namespace.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if ! command -v runway &> /dev/null
+then
+    echo "please install the runway cli first:"
+    echo "https://gitlab.eng.vmware.com/devtools/runway/cli/runctl/tree/master#installation"
+    exit 1
+fi
+
+
+config=~/.runway/config.yml
+if [ ! -f "$config" ]; then
+    echo "configuring runway cli creating: $config"
+    echo "please enter your vmware LDAP credentials"
+    read -p "username: " username
+    read -s -p "password: " password
+    mkdir -p ~/.runway
+    echo "auth: $(echo -n "${username}:${password}" | base64)" > $config
+    echo ""
+fi
+
+echo "Updating namespace"
+# bosh-core namespace has id 719
+runctl ns update --id=719 --access access.json
